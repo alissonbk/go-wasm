d6fd84d4312dea7fed04cfd941b6013a37283a2e
alissonbk <alissonbk2010@hotmail.com>
2024-07-06 15:53:34 -0300

    feat: circles with movement

--- main.go
+++ main.go
@@ -9,13 +9,27 @@ import (
 )
 
 var (
-	ctx                 js.Value
-	circleRadius              = 20
-	offset                    = 40
-	colorList                 = [...]string{"red", "green", "blue", "cyan", "pink"}
-	circleAmountAtFrame       = 20
-	FPS                 int64 = 60
+	colorList     = [...]string{"blue", "green", "yellow", "purple", "violet", "cyan"}
+	ctx           js.Value
+	startVelocity = Pos{x: 2, y: 6}
 )
+
+const (
+	screenOffset      int     = 40
+	FPS               int64   = 144
+	startCircleRadius float32 = 14
+	friction          float64 = 0.98
+	gravity           float64 = 0.08
+	fadeOutSpeed      float32 = 0.995
+	amountOfCircles   int     = 60
+)
+
+type Circle struct {
+	color    string
+	pos      Pos
+	velocity Pos
+	radius   float32
+}
 
 type Pos struct {
 	x float64
@@ -38,27 +52,58 @@ func getRandomColor() string {
 }
 
 func getRandomPosition(maxPos Pos) Pos {
-	maxX := int(maxPos.x) - (offset * 2)
-	maxY := int(maxPos.y) - (offset * 2)
+	maxX := int(maxPos.x) - (screenOffset * 2)
+	maxY := int(maxPos.y) - (screenOffset * 2)
 	return Pos{
-		x: float64(rand.Intn(maxX) + offset),
-		y: float64(rand.Intn(maxY) + offset),
+		x: float64(rand.Intn(maxX) + screenOffset),
+		y: float64(rand.Intn(maxY) + screenOffset),
 	}
 }
 
-func draw(doc js.Value, canvasEl js.Value, bodySize Pos) {
+func generateCircles(bodySize Pos) [amountOfCircles]Circle {
+	circles := [amountOfCircles]Circle{}
+
+	for i := range circles {
+		circles[i].pos = getRandomPosition(bodySize)
+		circles[i].color = getRandomColor()
+		circles[i].velocity = startVelocity
+		circles[i].radius = startCircleRadius
+	}
+	return circles
+}
+
+func drawArc(circle Circle) {
+	ctx.Call("beginPath")
+	ctx.Call("arc", circle.pos.x, circle.pos.y, circle.radius, 0, 2*math.Pi)
+	ctx.Set("fillStyle", circle.color)
+	ctx.Call("fill")
+	ctx.Call("closePath")
+}
+
+func updateCirclePosition(c *Circle, bodySize Pos) {
+	(*c).radius *= fadeOutSpeed
+	(*c).velocity.x *= friction
+	(*c).velocity.y *= friction
+	(*c).velocity.y += gravity
+
+	if c.pos.x < (bodySize.x/2)-float64(screenOffset) {
+		(*c).pos.x -= (*c).velocity.x
+	} else {
+		(*c).pos.x += (*c).velocity.x
+	}
+	(*c).pos.y += (*c).velocity.y
+}
+
+func draw(doc js.Value, canvasEl js.Value, bodySize Pos, circles *[amountOfCircles]Circle) {
 	handleWindowResize(doc, canvasEl, bodySize)
-
-	for i := 0; i < circleAmountAtFrame; i++ {
-		randomPos := getRandomPosition(bodySize)
-		ctx.Call("beginPath")
-		ctx.Call("arc", randomPos.x, randomPos.y, circleRadius, 0, 2*math.Pi)
-		ctx.Set("fillStyle", getRandomColor())
-		ctx.Call("fill")
+	for i := range circles {
+		updateCirclePosition(&circles[i], bodySize)
+		drawArc(circles[i])
 	}
+
 }
 
-//https://codepen.io/chriscourses/pen/Vwamprd
+// https://codepen.io/chriscourses/pen/Vwamprd
 
 func main() {
 	doc := js.Global().Get("document")
@@ -73,13 +118,14 @@ func main() {
 	canvasEl.Set("height", bodySize.y)
 
 	ctx = canvasEl.Call("getContext", "2d")
+	circles := generateCircles(bodySize)
 
 	timer := time.Now().UnixMilli()
 	var renderFrame js.Func
 	renderFrame = js.FuncOf(func(this js.Value, args []js.Value) interface{} {
 		now := time.Now().UnixMilli()
 		if now >= timer+(1000/FPS) {
-			draw(doc, canvasEl, bodySize)
+			draw(doc, canvasEl, bodySize, &circles)
 			timer = time.Now().UnixMilli()
 		}
 
